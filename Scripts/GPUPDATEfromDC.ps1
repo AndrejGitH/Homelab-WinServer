6386a06bff71a14d08c76b4f05da2aa341eac509
AndrejGitH <141548698+AndrejGitH@users.noreply.github.com>
2025-07-27 15:05:02 +0200

    Add files via upload

---
+++ Scripts/GPUPDATEfromDC.ps1
@@ -0,0 +1,8 @@
+﻿# This script runs gpupdate /force on specified computers.
+# Usage: Run from a domain-joined machine with remoting enabled.
+
+
+$computers = @("WinServerUi", "Sales-Smith")
+foreach ($computer in $computers) {
+    Invoke-Command -ComputerName $computer -ScriptBlock { gpupdate /force }
+}
