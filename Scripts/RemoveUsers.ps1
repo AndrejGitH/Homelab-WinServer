284dabfd964ca2fda3e5e3e796a700b5651e6bfa
AndrejGitH <141548698+AndrejGitH@users.noreply.github.com>
2025-08-04 14:59:43 +0200

    Add files via upload

---
+++ Scripts/RemoveUsers.ps1
@@ -0,0 +1,21 @@
+﻿Import-Module ActiveDirectory
+
+$usernames = @("cmalek")
+
+foreach($username in $usernames){
+
+try{
+
+
+Remove-ADUser -Identity $username -Confirm:$true
+Write-Host "User with username $username was successfully removed!"
+
+}
+
+catch {
+
+Write-Host "User with username $username could not be removed! Reason: $($_.Exception.Message)"
+
+}
+
+}
